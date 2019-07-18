f83aed4f05789a09b800b6bf13dfd92533f424c6
MasayaAoyama <tennis.tenmasa@gmail.com>
2019-07-18 22:11:51 +0900

    vi pkg/apis/servers/v1beta1/webserver_types.go # (スキーマの編集: spec.replicasとspec.contentの追加)

--- pkg/apis/servers/v1beta1/webserver_types.go
+++ pkg/apis/servers/v1beta1/webserver_types.go
@@ -25,8 +25,8 @@ import (
 
 // WebServerSpec defines the desired state of WebServer
 type WebServerSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Replicas int32  `json:"replicas,omitempty"`
+	Content  string `json:"content,omitempty"`
 }
 
 // WebServerStatus defines the observed state of WebServer
